10abceba917db4c31ab66db8cb4b13d1e5d015c6
Antonio Roman <antoniofroman@gmail.com>
2016-10-24 17:49:05 -0400

    Split a string into the integer and floating value.

--- Source.cpp
+++ Source.cpp
@@ -5,11 +5,20 @@
 // Main function for running program and testing binary conversions.
 int main()
 {
-	float floatInput = 0.2;
-	int input;
+	std::string input;
+	std::string integerBinary, fractionBinary, representation, s;
+	int integerPart, fractionPart;
 	
 	std::cout << "Floating Point Binary Program :\n\n";
+
+
 	std::cout << "Enter value for floating point: ";
 	std::cin >> input;
-	std::cout << "Binary Conversion : " << float2bin(input) << "\n";
+
+	// Separate Integer from Floating
+	integerPart = std::stoi(input.substr(0, input.find(".")));
+	fractionPart = std::stoi(input.substr(input.find(".") + 1));
+	std::cout << "integerPart = " << integerPart << "\n";
+	std::cout << "fractionPart = " << fractionPart << "\n";
+	std::cout << "Binary Conversion : " << float2bin(fractionPart) << "\n";
 }
